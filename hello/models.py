a3c6d9f8b4e98b4a92e69d5986ebee3ce64cdf1b
gondzio <gondzio00@gmail.com>
2017-11-08 10:02:20 +0100

    new model update

--- hello/models.py
+++ hello/models.py
@@ -5,5 +5,5 @@ class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
 class Post(models.Model):
-    title = models.CharField(max_length='250')
-    content = models.TextField(max_length='250')
+    title = models.CharField(max_length=250)
+    content = models.TextField(max_length=250)
