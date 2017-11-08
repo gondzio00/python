649c2a8b6ceb0b861f0d958c3984a4a220977479
gondzio <gondzio00@gmail.com>
2017-11-08 11:09:47 +0100

    s

---
+++ hello/forms.py
@@ -0,0 +1,34 @@
+import os
+import requests
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import PostForm
+
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
